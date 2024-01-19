31bd1bd53b2d7b4b05b4aa445f61d72ac229ffa7
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2024-01-19 11:02:37 +0200

    services

---
+++ CarsProject.AplicationServices/CarsServices.cs
@@ -0,0 +1,86 @@
+﻿using CarsProject.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsProject.AplicationServices
+{
+    public class CarsServices : ICarsServices
+    {
+        private readonly CarsProjectContext _context;
+
+        public CarsServices
+            (
+                CarsProjectContext context
+            )
+        {
+            _context = context;
+        }
+        public async Task<Car> Create(CarDto dto)
+        {
+            Car car = new Car();
+
+            car.Id = Guid.NewGuid();
+            car.Make = dto.Make;
+            car.Model = dto.Model;
+            car.Year = dto.Year;
+            car.Color = dto.Color;
+            car.MotorPower = dto.MotorPower;
+            car.Fuel = dto.Fuel;
+            car.Price = dto.Price;
+
+
+            car.CreatedAt = DateTime.Now;
+            car.UpdatedAt = DateTime.Now;
+
+
+            await _context.Cars.AddAsync(car);
+            await _context.SaveChangesAsync();
+
+            return car;
+        }
+
+        public async Task<Car> Delete(Guid id)
+        {
+            var carId = await _context.Cars
+               .FirstOrDefaultAsync(x => x.Id == id);
+
+            _context.Cars.Remove(carId);
+            await _context.SaveChangesAsync();
+
+            return carId;
+        }
+
+        public async Task<Car> GetAsync(Guid id)
+        {
+            var result = await _context.Cars
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return (result);
+        }
+
+        public async Task<Car> Update(CarDto dto)
+        {
+
+            var domain = new Car();
+
+            domain.Id = dto.Id;
+            domain.Make = dto.Make;
+            domain.Model = dto.Make;
+            domain.Year = dto.Year;
+            domain.Color = dto.Color;
+            domain.MotorPower = dto.MotorPower;
+            domain.Fuel = dto.Fuel;
+            domain.Price = dto.Price;
+
+            domain.CreatedAt = DateTime.Now;
+            domain.UpdatedAt = DateTime.Now;
+
+            _context.Cars.Update(domain);
+            await _context.SaveChangesAsync();
+
+            return domain;
+        }
+    }
+}
---
+++ CarsProject.Core/ServiceInterface/ServiceInterface.cs
@@ -0,0 +1,18 @@
+﻿using CarsProject.Core.Domain;
+using CarsProject.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsProject.Core.ServiceInterface
+{
+    public interface IServiceInterface
+    {
+        Task<Car> Create(CarDto dto);
+        Task<Car> GetAsync(Guid id);
+        Task<Car> Delete(Guid id);
+        Task<Car> Update(CarDto dto);
+    }
+}
