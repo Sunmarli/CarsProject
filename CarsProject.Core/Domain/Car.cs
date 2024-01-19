517f9d05f23b3a623149a794791d68df374881d2
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2024-01-19 10:52:59 +0200

    domain and dto added

---
+++ CarsProject.Core/Domain/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsProject.Core.Domain
+{
+    public class Car
+    {
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public string MotorPower { get; set; }
+        public string Fuel { get; set; }
+        public string Price { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
