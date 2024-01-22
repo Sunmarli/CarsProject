f442c1155930256e6acb524a57bba4326af7b209
Maria Jarv <118188337+Sunmarli@users.noreply.github.com>
2024-01-22 16:23:49 +0200

    added nugedpackages for test

--- CarsTest/Mock/MockHostEnvironment.cs
+++ CarsTest/Mock/MockHostEnvironment.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 
 namespace CarsTest.Mock
 {
